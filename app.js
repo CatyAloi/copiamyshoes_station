5cd5fcfe4ecfdcc64988f77a3ac848e810b8a01a
Caty Aloi <catyaloi@gmail.com>
2021-12-27 21:16:08 -0300

    creando archivos nuevos

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve (__dirname, './public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/home.html')); });
+
+app.get('/detalleproducto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/detalle_producto.html')); });
+
+app.get('/catalogoproducto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/catalogo_producto.html')); });
+
+app.get('/contacto', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_contact.html')); });
+
+app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_login.html')); });
+
+app.get('/registro', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/form_register.html')); });
+
+app.get('/carrito', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/cart.html')); });
+
+app.listen(3000, () => console.log('Servidor Shoes_Station corriendo'));
